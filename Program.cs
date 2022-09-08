680f3cf5805b318c371b4c424dcc6a16e1a9b065
Sophia <sophia852686@gmail.com>
2022-09-08 19:02:46 +1000

    finish data types

--- Program.cs
+++ Program.cs
@@ -1,58 +1,201 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, Sophia!");
-
 // Console.WriteLine("I JUST DID IT");
-
 using System;
 
 namespace HelloWorld
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Hello Sophia!");  
-      //Console.WriteLine("How are you today?");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Sophia!");
 
-      //take input from user
-      //string input = Console.ReadLine();
-      //Console.WriteLine($"I am {input} today.");
+            //Console.WriteLine("How are you today?");
+            //take input from user
+            //string input = Console.ReadLine();
+            //Console.WriteLine($"I am {input} today.");
+            // Create Variables
+            string name = "Shadow";
+            string breed = "Golden Retriever";
 
-      // Create Variables
-      string name ="Shadow";
-      string breed ="Golden Retriever";
-     /*  int age=5;
-      double weight = 65.22;
-      bool spayed = true; */
-      // Print variables to the console
-      Console.WriteLine($"{name} {breed}");
-      // Console.WriteLine(breed);
-      // Console.WriteLine(age);
-      // Console.WriteLine(weight);
-      // Console.WriteLine(spayed);
+            /*  int age=5;
+            double weight = 65.22;
+            bool spayed = true; */
+            // Print variables to the console
+            Console.WriteLine($"{name} {breed}");
 
-    
-    //Converting Data Types
-      int myInt = 3;
-     // Turn it into a decimal
-     double myDouble = myInt;
-     Console.WriteLine(myDouble);
-    
-    // Round myDouble to the nearest whole number
-    double myDouble2 = 4.9;
-    int myInt2 = (int)myDouble2;
-    Console.WriteLine(myInt2);
+            // Console.WriteLine(breed);
+            // Console.WriteLine(age);
+            // Console.WriteLine(weight);
+            // Console.WriteLine(spayed);
+            //Converting Data Types
+            int myInt = 3;
 
+            // Turn it into a decimal
+            double myDouble = myInt;
+            Console.WriteLine (myDouble);
 
-    // Ask user for fave number, default will be string
-      Console.Write("Enter your favorite number: ");
-      
-    //Convert string intput into an int:
-     int faveNumber = Convert.ToInt32(Console.ReadLine());
-     Console.WriteLine($"My favorite number is {faveNumber}.");
-    
+            // Round myDouble to the nearest whole number
+            double myDouble2 = 4.9;
+            int myInt2 = (int) myDouble2;
+            Console.WriteLine (myInt2);
 
-     }
-  }
-};
- 
+            // Ask user for fave number, default will be string
+            Console.Write("Enter your favorite number: ");
+
+            //Convert string intput into an int:
+            int faveNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"My favorite number is {faveNumber}.");
+
+            /* Numerical Data Types: int, decimal,double */
+            // Number of pizza shops use int data type
+            int pizzaShops = 4332;
+
+            // use decimal data type as floate but end with "m"
+            decimal revenue = 390819.28m;
+
+            // Log the values to the console:
+            Console.WriteLine (pizzaShops);
+            Console.WriteLine (revenue);
+
+            /* Arithmetic Operators */
+            // Your Age
+            int userAge = 19;
+
+            // Length of years on Jupiter (in Earth years)
+            double jupiterYears = 11.86;
+
+            // Age on Jupiter
+            double jupiterAge = userAge / jupiterYears;
+            Console.WriteLine (jupiterAge);
+
+            // Time to Jupiter
+            double journeyToJupiter = 6.142466;
+
+            // New Age on Earth
+            double newEarthAge = userAge + journeyToJupiter;
+
+            // New Age on Jupiter
+            double newJupiterAge = newEarthAge / jupiterYears;
+
+            // Log calculations to console
+            Console.WriteLine (newEarthAge);
+            Console.WriteLine (newJupiterAge);
+
+            /* Modulo */
+            // Number of students
+            int students = 18;
+
+            // Number of students in a group
+            int groupSize = 3;
+
+            // Does groupSize go evenly into students?
+            Console.WriteLine(students % groupSize);
+
+            /* Built-In Methods */
+            // Starting variables
+            int numberOne = 12932;
+            int numberTwo = -2828472;
+
+            // ind the square root of numberOne and
+            //round the answer down so it doesn’t have a decimal.
+            double numberOneSqrt = Math.Floor(Math.Sqrt(numberOne));
+
+            // The built-in method Math.Sqrt() can only take a positive number
+            //as a value, but the value of numberTwo is negative.
+            //Math.Floor(Math.Sqrt(Math.Abs(number)));
+            double numberTwoSqrt = Math.Floor(Math.Sqrt(Math.Abs(numberTwo)));
+
+            // Print the lowest number
+            Console.WriteLine(Math.Min(numberOneSqrt, numberTwoSqrt));
+
+            // Raise numberOne to the numberTwo power (2*2*2)
+            Console.WriteLine(Math.Pow(2, 3));
+
+            // Round number up
+            Console.WriteLine(Math.Ceiling(2.4));
+
+            // Find the larger number between two numbers
+            Console.WriteLine(Math.Max(12, 34));
+
+            // First string variable
+            string firstSentence =
+                "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.";
+
+            // Second string variable
+            string firstSpeech =
+                "\"My dear Mr. Bennet,\" said his lady to him one day, \"have you heard that Netherfield Park is let at last?\"";
+
+            // Print variable and newline
+            Console.WriteLine (firstSentence);
+            Console.WriteLine("\n");
+            Console.WriteLine (firstSpeech);
+
+            // Declare the variables
+            string beginning = "Once upon a time,";
+            string middle = "The kid climbed a tree";
+            string end = "Everyone lived happily ever after.";
+
+            // Interpolate the string and the variables
+            string story = $"{beginning} {middle} {end}";
+
+            // Print the story to the console
+            Console.WriteLine (story);
+
+            // Create password
+            string password = "a92301j2add";
+
+            // Get password length
+            int passwordLength = password.Length;
+
+            // Check if password uses symbol
+            int passwordCheck = password.IndexOf("!");
+
+            // If IndexOf()doesn’t exist in the string the method will return a -1
+            Console.WriteLine($"The user password is {password}.");
+            Console.WriteLine($"Its length is {passwordLength}");
+            Console.WriteLine($" receives a {passwordCheck} check");
+
+            // Get Parts of Strings
+            string startStrand = "ATGCGATGAGCTTAC";
+
+            // find location of "tga"
+            int tga = startStrand.IndexOf("TGA");
+
+            // start point and stop point variables
+            int startPoint = 0;
+            int length = tga + 3;
+
+            // .Substring() grabs part of a string using
+            //the specified character position, continues until the end of the string,
+            string dna = startStrand.Substring(startPoint, length);
+            Console.WriteLine (dna);
+
+            // DNA mutation search
+            Console.WriteLine(dna[3]);
+
+            // Script line
+            string script =
+                "Close on a portrait of the HANDSOME PRINCE -- as the BEAST'S giant paw slashes it.";
+
+            // Get camera directions
+            int charPosition = script.IndexOf("Close");
+            int length2 = "Close on".Length;
+            string cameraDirections = script.Substring(charPosition, length2);
+
+            // Get scene description
+            charPosition = script.IndexOf("a portrait");
+            string sceneDescription = script.Substring(charPosition);
+
+            // Make camera directions uppercase
+            cameraDirections = cameraDirections.ToUpper();
+
+            // Make scene description lowercase
+            sceneDescription = sceneDescription.ToLower();
+
+            // Print results
+            Console.WriteLine(cameraDirections + sceneDescription);
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -143,3 +143,183 @@ It’s also possible to convert data types using built-in methods. For most data
 One example of when we have to use conversions is when we ask a user to input a numerical value. Even if that value is an integer or a decimal, `Console.ReadLine() will always return a string.`
 
 [How to convert a string to a number (C# Programming Guide)](https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/types/how-to-convert-a-string-to-a-number)
+
+---
+## Numerical Data Types
+
+Let’s look at two data types that we can use to represent different numerical values:
+
+- To define a variable with the type int, you would write it as follows:
+
+`int variableName = 7;`
+
+- Double and Decimal
+
+If we need to use a decimal value, we have a few options: float, double, and decimal. These values are useful for anything that requires more precision than a whole number, like measuring the precise location of an object in 3D space.
+
+A double is usually the best choice of the three because it is more precise than a float, but faster to process than a decimal. However, make sure to use a decimal for financial applications, since it is the most precise.
+
+- To define a variable with the type double, you would write it as follows:
+
+`double variableName = 39.76876;`
+
+
+- To define a variable with the type decimal, you would write it as follows: Don’t forget the m character after the number! This character tells C# that we’re defining a decimal and not a double.
+  
+`decimal variableName = 489872.76m;`
+
+---
+## Arithmetic Operators
+Arithmetic operators include:
+addition `+`
+subtraction `-`
+multiplication `*`
+division `/`
+
+We can use these symbols to perform operations on numbers and create new values.
+
+```
+int answer = 4 + 19;
+Console.Write(answer);
+ 
+// prints 23
+```
+---
+## Modulos 
+
+Modulos  are useful because they let us know if a number “fits” into a larger number, or if there will be a remainder. For example, how many eggs will be left over if I try and fit 56 eggs into crates of a dozen eggs?
+
+```
+int eggs = 56;
+int crateAmount = 12;
+ 
+int eggsLeftOver = eggs % crateAmount; 
+Console.Write(eggsLeftOver); // prints 8
+```
+
+
+It can also be used to check if a number is odd or even. If a number is even, taking its modulo with 2 it will return a 0 and if it is odd it will return a 1:
+
+```
+int myNum = 85939824;
+Console.Write(myNum % 2); // prints 0, so number is even
+```
+---
+### Built-In Methods
+There are several built-in methods that we can use to manipulate numerical data and perform more complex mathematical calculations. Here are a few:
+
+- `Math.Abs()`—will find the absolute value of a number. Example: `Math.Abs(-5)` returns 5.
+- `Math.Sqrt()`—will find the square root of a number. Example: `Math.Sqrt(16)` returns 4.
+- `Math.Floor()`—will round the given double or decimal down to the nearest whole number. Example: `Math.Floor(8.65)` returns 8.
+- `Math.Min()`—returns the smaller of two numbers. Example: `Math.Min(39, 12)` returns 12.
+
+
+---
+## Introduction to Working with Text
+
+A string is a group of characters surrounded by quotation marks, like "https://codecademy.com" or "To be or not to be." A string is just a collection of a smaller data type, char, which is a single character like “a” or “?”.
+
+To define a variable as a string, you write the data type, then the variable name. Then set it equal to the value, which is inside of quotation marks :
+`string variableName = "puppy";`
+
+#### [Escape Character Sequences](https://docs.microsoft.com/en-us/cpp/c-language/escape-sequences?view=msvc-170&viewFallbackFrom=vs-2017)
+An escape sequence places a backslash (\) before the inner quotation marks, so the program doesn’t read them accidentally as the end of sequence.
+
+```
+string withoutSlash = "Ifemelu said, "Hello!"";
+ 
+string withSlash = "Ifemelu said, \"Hello!\"";
+
+string newLine = "Ifemelu walked \n to the park.";
+```
+
+---
+## String Concatenation
+A common way to do is by using string concatenation. String concatenation is when we combine strings using the addition symbol `(+)`, literally adding one string to another.
+
+```
+string yourFaveMusician = "David Bowie";
+string myFaveMusician = "Solange";
+ 
+Console.WriteLine("Your favorite musician is " + yourFaveMusician + " and mine is " + myFaveMusician + ".");
+```
+---
+## [String Interpolation](https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/tokens/interpolated)
+ String interpolation was introduced in C# 6 and it enables us to insert our variable values and expressions in the middle of a string, without having to worry about spaces and punctuation.
+
+```
+string yourFaveMusician = "David Bowie";
+string myFaveMusician = "Solange";
+ 
+Console.WriteLine($"Your favorite musician is {yourFaveMusician} and mine is {myFaveMusician}.")
+```
+
+---
+## Get Info About Strings
+- Length
+  - Since strings are composed of a set of characters, we can find out how many characters exist in a string with the `.Length` method.
+
+```
+string userTweet = Console.ReadLine();
+ 
+userTweet.Length; // returns the length of the tweet
+```
+
+- IndexOf
+
+We can also find the position of a specific character or substring using .`IndexOf()`. This method is useful for searching to see if something exists in a string.
+
+```
+string word = "radio";
+ 
+word.IndexOf("a"); // returns 1
+```
+Since positioning starts at 0, the second thing in the string will return a 1. If it doesn’t exist in the string the method will return a -1. If we pass it an empty string, it will return 0. If it occurs more than once, it will return the first instance.
+
+---
+## Get Parts of Strings
+We can also use built-in .NET methods to grab parts of strings or specific characters in a string.
+
+#### Substring
+`.Substring()` grabs part of a string using the specified character position, continues until the end of the string, and returns a new string. `.IndexOf()` is usually used first to get the specific character position.
+
+```
+string plantName = "Cactaceae, Cactus"; 
+int charPosition = plantName.IndexOf("Cactus"); // returns 11
+string commonName = plantName.Substring(charPosition); // returns Cactus
+```
+
+`.Substring()` is useful if we only want to use part of a string but keep the original data intact. So in this instance, we want to keep the string plantName, but just grab the `"Cactus"` portion of the string. We use `.IndexOf()` to find where `"Cactus"` starts, then use .`Substring()` with the position information to save "Cactus" to the new variable commonName.
+
+
+We can also pass `.Substring() `a second argument, which will determine the number of characters in the resulting substring. For example, the following code shows how we can use `.Substring()` with two arguments to **specify the length of our substring**:
+
+```
+string name = "Codecademy"; 
+int start = 2;
+int length = 6;
+string substringName = name.Substring(start, length); // returns 'decade'
+```
+
+#### Bracket Notation
+Bracket notation is a style of syntax that uses brackets `[]` and an integer value to identify a particular value in a collection. In this case, we can use it to find a specific character in a string.
+
+```
+string plantName = "Cactaceae, Cactus";
+int charPosition = plantName.IndexOf("u"); // returns 15
+char u = plantName[charPosition]; // returns u
+```
+
+Similar to the example above, we first use `.IndexOf()` to grab the character position, which in this case is 15. We then take the string value and append it with a set of brackets `[]` and place the `charPosition` value inside the brackets.
+
+---
+## Manipulate Strings
+There are also built-in .NET methods that we can use to manipulate text data. Using these methods on a string doesn’t change the string itself, but creates an entirely new one.
+
+We can quickly change the case of our strings using the methods `.ToUpper()` and `.ToLower()`. These methods are useful if we want to make our text look like an e.e. cumming’s poem or make it appear like we forgot to turn off our caps lock key.
+
+```
+string shouting = "I'm not shouting, you're shouting".ToUpper();
+Console.WriteLine(shouting);
+// prints I'M NOT SHOUTING, YOU'RE SHOUTING.
+```
